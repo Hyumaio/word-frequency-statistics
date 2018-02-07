040084f1f41b1d4b7bf756b8d7abd1c97053c529
Hyumaio <mayuyu4c@foxmail.com>
2018-02-07 14:15:07 +0800

    Use collections.Counter class in v2.

--- README.md
+++ README.md
@@ -5,4 +5,10 @@
 This program is for word frequency statistcs, only function on a utf-8 encoding's file, it analysis the file
 content and print the first n(customizable) word matches with its appear times in the console.
 
+### WordFrequencyStatitics:
+Complete with customized class.
+
+### WordFrequencyStatitics_v2:
+Complete with collections.Counter class.
+
 Compatible with Python2.
---
+++ WordFrequencyStatitics_v2.py
@@ -0,0 +1,49 @@
+import io
+import re
+
+from collections import Counter
+
+FILE_PATH = r'C:\Users\supermiao\Documents\~\md\David Goodger.md'
+REGULAR_EXPRESS = r'[A-Za-z]+'
+
+
+class WordFrequencyStatistics(object):
+    def __init__(self, file, regex):
+        """
+        Generate a tuple, its first element is a word, the second is its appear times.
+
+        :param file: File path from global variable FILE_PATH, make sure the file encoding is utf-8.
+        :param regex: Regular express from global variable REGULAR_EXPRESS.
+        """
+        self.file = file
+        self.regex = regex
+
+        with io.open(self.file, encoding='utf-8') as f:
+            # use io.open instead of open which is only functional in Python 3.
+            data = f.read()
+            word_list = [single.lower() for single in re.findall(self.regex, data)]
+
+            # Define a dictionary, key-value: <word>-<times>.
+            statistics_dict = dict()
+
+            # Iterate word_list to see if there's a match word, if match failed create a key-value pairs
+            # as <word>-0 in statistics_dict, otherwise make its times plus 1.
+            for word in word_list:
+                statistics_dict[word] = statistics_dict.get(word, 0) + 1
+            self.mapping = statistics_dict
+
+    def sort_mapping(self, n=5):
+        """
+        Return first <n> words of the sorted tuple.
+
+        :param n: How many words should be return, default 5.
+        """
+        counter = Counter(self.mapping)
+        return counter.most_common(n)
+
+
+if __name__ == '__main__':
+    statistics = WordFrequencyStatistics(FILE_PATH, REGULAR_EXPRESS)
+    result = statistics.sort_mapping()
+    for item in result:
+        print(item)
